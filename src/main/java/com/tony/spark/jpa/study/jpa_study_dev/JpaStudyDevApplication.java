b680da30de4ad1f6c80f723bb7b89894cb9030f3
eves <evesjapan@gmail.com>
2018-06-12 02:32:29 +0900

    #Setting JPA

---
+++ src/main/java/com/tony/spark/jpa/study/jpa_study_dev/JpaStudyDevApplication.java
@@ -0,0 +1,12 @@
+package com.tony.spark.jpa.study.jpa_study_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStudyDevApplication.class, args);
+	}
+}
